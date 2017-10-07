42451e3d9737e0aafc43634fcffc78e739ac2f58
Michał Sałaban <michal@salaban.info>
2017-10-07 19:15:24 +0200

    Add 0MQ bind ports

--- node3/run.sh
+++ node3/run.sh
@@ -7,6 +7,7 @@ monerod --testnet --no-igd --hide-my-port --testnet-data-dir $DIR \
 	--p2p-bind-ip 127.0.0.1 \
 	--testnet-p2p-bind-port 48080 \
 	--testnet-rpc-bind-port 48081 \
+	--zmq-testnet-rpc-bind-port 48082 \
 	--add-exclusive-node 127.0.0.1:28080 \
 	--add-exclusive-node 127.0.0.1:38080 \
 	--start-mining 9u9j6xG1GNu4ghrdUL35m5PQcJV69YF8731DSTDoh7pDgkBWz2LWNzncq7M5s1ARjPRhvGPX4dBUeC3xNj4wzfrjV6SY3e9 \
